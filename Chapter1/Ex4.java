3facec6783099fd31c01d6999c894d9d87cd1949
Syed Nazmul Islam <syednazmulislamramim@gmail.com>
2024-11-18 23:13:39 +0600

    Create Ex4.java

---
+++ Chapter1/Ex4.java
@@ -0,0 +1,11 @@
+public class Ch1Ex4 {
+    public static void main(String[] args) {
+        // have to print a table  which will conatain
+        // the main,square and cubic values of numbers from 1 to 5
+        System.out.println("a   a^2   a^3");
+        for(int i=1;i<=4;i++)
+        {
+            System.out.println(i+"    "+i*i+"    "+i*i*i);
+        }
+    }
+}
