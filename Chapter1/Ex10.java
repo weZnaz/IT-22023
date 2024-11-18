ad8d67dbd28704af4a3029074470931283c28938
Syed Nazmul Islam <syednazmulislamramim@gmail.com>
2024-11-18 23:11:37 +0600

    Create Ex10.java

---
+++ Chapter1/Ex10.java
@@ -0,0 +1,8 @@
+public class Ch1Ex10{
+    public static void main(String[] args){
+        //speed in miles/hour
+        double dis_km = 14 , minutes = 45.5;
+        double dis_mile = 14.0/1.6 , hours = 45.5/60;
+        System.out.println("Speed in miles/hour = " + dis_mile/hours);   
+    }
+}
