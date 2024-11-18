64f548cf41b0c78d405ecfcc8361d1ff4dd5d60d
Syed Nazmul Islam <syednazmulislamramim@gmail.com>
2024-11-18 23:13:19 +0600

    Create Ex8.java

---
+++ Chapter1/Ex8.java
@@ -0,0 +1,8 @@
+public class Ch1Ex8{
+    public static void main(String[] args){
+        //area and perimeter of a circle
+        double radius = 5.5;
+        System.out.println("Area of circle = " + radius * radius * 3.1416);
+        System.out.println("Perimeter of circle = " + 2 * radius * 3.1416);
+    }
+}
