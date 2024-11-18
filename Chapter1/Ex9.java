44728c09c3d4fc5e28db6b0d7cdaa50c756cff76
Syed Nazmul Islam <syednazmulislamramim@gmail.com>
2024-11-18 23:12:52 +0600

    Create Ex9.java

---
+++ Chapter1/Ex9.java
@@ -0,0 +1,8 @@
+public class Ch1Ex9{
+    public static void main(String[] args){
+        //area and perimeter of a rectangle
+        double width = 4.5 , height = 7.9;
+        System.out.println("Area of circle = " + width * height);
+        System.out.println("Perimeter of circle = " + 2 * (width + height));
+    }
+}
